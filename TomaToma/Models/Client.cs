b289d8f89a63e9be8b11054e5f765a247854d075
mya7 <mondi32323@gmail.com>
2023-11-21 13:23:53 +0500

    21.11

---
+++ TomaToma/Models/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TomaToma.Models;
+
+public partial class Client
+{
+    public int Id { get; set; }
+
+    public string? Surname { get; set; }
+
+    public string? Name { get; set; }
+}
